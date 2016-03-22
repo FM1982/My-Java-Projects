7235c34fdc79c9be299ff5e348e5aaece8793069
FM1982 <mluensmann@uni-bremen.de>
2016-03-22 16:26:06 +0100

    Added files via upload

---
+++ ExitAction.java
@@ -0,0 +1,34 @@
+package mainfiles;
+
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+
+public class ExitAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ExitAction(){
+		super("Exit");
+		putValue(Action.MNEMONIC_KEY, new Integer(KeyEvent.VK_Q));
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		for(Frame mCFrame : JFrame.getFrames()){
+			if(mCFrame.isActive()){
+				WindowEvent mCWindowClosing = new WindowEvent(mCFrame, WindowEvent.WINDOW_CLOSING);
+				mCFrame.dispatchEvent(mCWindowClosing);
+			}
+		}		
+	}
+
+}
